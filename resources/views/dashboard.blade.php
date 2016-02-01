8efa02bdd2550d6979a7d3319d6f4c7e404c3f0c
Pri Anton Subardio <prianton@MacBook-Pro-Pri.local>
2016-02-02 00:56:20 +0700

    desain bootstrap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 <!-- Main component for a primary marketing message or call to action -->
 <div class="jumbotron">
   <h1>Selamat Datang</h1>
-  <p>Silahkan gunakan menu di atas untuk mengelola data.</p>
+  <p>Silahkan gunakan menu di atas untuk mengelola data BMT EL Sejahtera.</p>
   <p>
     <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
   </p>
